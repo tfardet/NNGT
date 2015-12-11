fef4bfd1c1b5b9d9ae17db2640ac606928c601b8
Silmathoron <Silmathoron@users.noreply.github.com>
2015-12-11 23:33:12 +0100

    mend

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
         name='nngt',
         version = '0.4a',
         description = 'Package to study growth and activity of neural networks',
-        package_dir={'nngt': 'src/nngt'},
-        packages = find_packages(),
+        packages = find_packages('src'),
         
         # Include the non python files:
         package_data = { '': ['*.txt', '*.rst', '*.md', '*.plt'] },
