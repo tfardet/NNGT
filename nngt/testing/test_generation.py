fa04a5f4059ba79094f26b90881d9adc7cb7ba0f
Silmathoron <Silmathoron@users.noreply.github.com>
2017-02-22 23:37:01 +0100

    added test for gaussian degree

--- nngt/testing/test_generation.py
+++ nngt/testing/test_generation.py
@@ -38,12 +38,14 @@ def _get_connections(instruct):
     #~ weighted = instruct.get("weighted", True))
     return nodes, density, edges, average_degree, directed, reciprocity
 
+
 def _fixed_deg_theo(instruct):
     fixed_degree = instruct["degree"]
     avg_free_degree = fixed_degree
     deg_type = instruct["degree_type"]
     std_free_degree = np.sqrt(avg_free_degree) if deg_type != "total" else 0
     return fixed_degree, avg_free_degree, std_free_degree
+
 
 def _fixed_deg_exp(graph, instruct):
     n, d, e, a_deg, directed, weighted = _get_connections(instruct)
@@ -63,32 +65,60 @@ def _fixed_deg_exp(graph, instruct):
         std_free_degree = np.std(degrees)
     return fixed_degree, avg_free_degree, std_free_degree
 
+
 def _gaussian_deg_theo(instruct):
-    pass
+    avg_degree = instruct["avg"]
+    std_degree = instruct["std"]
+    avg_free_degree = avg_degree
+    deg_type = instruct["degree_type"]
+    std_free_degree = np.sqrt(avg_free_degree)
+    return avg_degree, std_degree, avg_free_degree, std_free_degree
+
 
 def _gaussian_deg_exp(graph, instruct):
-    pass
+    n, d, e, a_deg, directed, weighted = _get_connections(instruct)
+    deg_type = instruct["degree_type"]
+    deg_types = [ 'in', 'out' ]
+    degrees = graph.get_degrees(deg_type, use_weights=False)
+    avg_degree = np.average(degrees)
+    std_degree = np.std(degrees)
+    # compute free_degree properties
+    avg_free_degree, std_free_degree = avg_degree, std_degree
+    if deg_type != 'total':
+        del deg_types[deg_types.index(deg_type)]
+        degrees = graph.get_degrees(deg_types[0], use_weights=False)
+        avg_free_degree = np.average(degrees)
+        std_free_degree = np.std(degrees)
+    return avg_degree, std_degree, avg_free_degree, std_free_degree
+
 
 def _erdos_renyi_theo(instruct):
     pass
 
+
 def _erdos_renyi_exp(graph, instruct):
     pass
+
 
 def _random_scale_free_theo(instruct):
     pass
 
+
 def _random_scale_free_exp(graph, instruct):
     pass
+
 
 def _newman_watts_theo(instruct):
     pass
 
+
 def _newman_watts_exp(graph, instruct):
     pass
 
+
 def _distance_rule_theo(instruct):
     pass
+
 
 def _distance_rule_exp(graph, instruct):
     pass
@@ -142,6 +172,7 @@ class TestGeneration(TestBasis):
         the expected properties are indeed obtained.
         '''
         graph_type = instructions["graph_type"]
+        print(graph_type)
         ref_result = self.theo_prop[graph_type](instructions)
         computed_result = self.exp_prop[graph_type](graph, instructions)
         self.assertTrue(np.allclose(
