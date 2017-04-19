6ea5432c276b4aec6f1aa54a63e2d3c06a6b6f7f
Silmathoron <silmathoron@laposte.net>
2017-04-19 13:10:39 +0200

    corrected warnings error in plot init

--- nngt/plot/__init__.py
+++ nngt/plot/__init__.py
@@ -55,6 +55,7 @@ if _nngt._config["graph_library"] == 'graph-tool':
     from .plt_networks import draw_network
     __all__.append("draw_network")
 else:
-    warnings.warn("Graph drawing is only available with graph_tool at the \
-moment. As {} is currently being used, all graph drawing functions will be \
-disabled.".format(_nngt._config["graph_library"]))
+    _warn.warn("Graph drawing is only available with graph_tool at the "
+               "moment. As {} is currently being used, all graph drawing "
+               "functions will be disabled.".format(
+               _nngt._config["graph_library"]))
