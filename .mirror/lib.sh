615162f0ee39c3dac48fbf268800868bf28241bf
Tanguy Fardet <tanguyfardet@protonmail.com>
2021-02-16 16:57:41 +0100

    moved back to single build file, removed test for branch type

--- .mirror/lib.sh
+++ .mirror/lib.sh
@@ -3,7 +3,7 @@ ensure_ssh() {
   # ensure our SSH allows us to talk to GitHub.
   # Even if we ended up talking to a malicious host, all we'd do is push our
   # open source code to them, so no real concern there.
-  KNOWN_HOSTS_FILE="$HOME/.ssh/known_hosts"
+  KNOWN_HOSTS_FILE="~/.ssh/known_hosts"
   touch "$KNOWN_HOSTS_FILE"
   chmod 0600 "$KNOWN_HOSTS_FILE"
   ssh-keyscan github.com > "$KNOWN_HOSTS_FILE"
