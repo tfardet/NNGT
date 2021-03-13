ac3f2f71e6710bb83a4a5da0825749d04e62b168
Tanguy Fardet <tanguyfardet@protonmail.com>
2021-03-13 13:10:52 +0100

    formatted headers

--- nngt/geography/__init__.py
+++ nngt/geography/__init__.py
@@ -1,3 +1,25 @@
+#-*- coding:utf-8 -*-
+#
+# geography/__init__.py
+#
+# This file is part of the NNGT project, a graph-library for standardized and
+# and reproducible graph analysis: generate and analyze networks with your
+# favorite graph library (graph-tool/igraph/networkx) on any platform, without
+# any change to your code.
+# Copyright (C) 2015-2021 Tanguy Fardet
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 from .countries import (maps, country_names, country_codes, convertors, cities,
                         codes_to_names)
 from .plot import draw_map
